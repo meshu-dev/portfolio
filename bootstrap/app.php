9cbd0856a911c2cb6bbface0bb8277d4b564116f
Mesh <harmeshuppal@gmail.com>
2024-08-06 17:40:58 +0100

    Revert cors middleware addition

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use([
-            \Illuminate\Http\Middleware\HandleCors::class
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Exception $exception, Request $request) {
