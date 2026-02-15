aec1983947670beaff35918efa1b4dc45e75d9a3
Mesh <contact@devpush.io>
2026-02-15 13:59:59 +0000

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 Route::livewire('/login', 'pages::auth.login')->name('login');
 
 Route::middleware(['auth:web'])->group(function () {
-    Route::livewire('/', 'pages::users.index');
-    Route::livewire('/profile', 'pages::cv.profile');
-    Route::livewire('/skills', 'pages::cv.skills');
-    Route::livewire('/logout', 'pages::auth.logout');
+    Route::livewire('/',                      'pages::cv.profile');
+    Route::livewire('/skills',                'pages::cv.skills');
+    Route::livewire('/work-experiences',      'pages::cv.work-experiences');
+    Route::livewire('/work-experiences/{id}', 'pages::cv.work-experience');
+    Route::livewire('/logout',                'pages::auth.logout');
+
+    Route::livewire('/hello',                      'pages::users.index');
 });
