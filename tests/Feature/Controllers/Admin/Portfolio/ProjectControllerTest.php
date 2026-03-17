943e60318eaecf78087838c2317cbc78b055b970
Mesh <contact@devpush.io>
2026-03-17 21:52:42 +0000

    Add portfolio unit test files

---
+++ tests/Feature/Controllers/Admin/Portfolio/ProjectControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Enums\{FlashTypeEnum, UserEnum};
+use App\Models\{Profile, User};
+use Inertia\Testing\AssertableInertia as Assert;
+
+describe('ProjectController tests', function () {
+
+});
---
+++ tests/Feature/Controllers/Admin/Portfolio/RepositoryControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Enums\{FlashTypeEnum, UserEnum};
+use App\Models\{Profile, User};
+use Inertia\Testing\AssertableInertia as Assert;
+
+describe('RepositoryController tests', function () {
+
+});
