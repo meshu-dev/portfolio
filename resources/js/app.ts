6846fef32349c4aeed5a62e45df29173e62dac9d
Mesh <contact@devpush.io>
2026-04-14 19:08:51 +0100

    Add layout

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,6 @@
 import { createInertiaApp } from '@inertiajs/vue3';
+import AuthLayout from './layouts/AuthLayout.vue';
+import UserLayout from './layouts/UserLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -6,5 +8,12 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     progress: {
         color: '#4B5563',
+    },
+    layout: (name) => {
+        if (name.startsWith('login')) {
+            return AuthLayout
+        }
+
+        return UserLayout
     },
 });
