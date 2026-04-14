3649c41d295077a8db889dcc4ac862f5aa68a522
Mesh <contact@devpush.io>
2026-04-14 18:12:32 +0100

    A

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,22 +1,10 @@
-import { createInertiaApp } from '@inertiajs/vue3'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import type { DefineComponent } from 'vue'
-import { createApp, h } from 'vue'
-import '../css/app.css'
-//import AuthLayout from './layouts/AuthLayout.vue'
-//import UserLayout from './layouts/UserLayout.vue'
-import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./pages/**/*.vue')
-    return pages[`./pages/${name}.vue`]()
-  },
-  setup({ el, App, props, plugin }) {
-      createApp({ render: () => h(App, props) })
-          .use(plugin)
-          .mount(el)
-  },
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    progress: {
+        color: '#4B5563',
+    },
 });
