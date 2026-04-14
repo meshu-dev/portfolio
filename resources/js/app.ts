9d622701e153f0f7b9c1064d9a037df5f7578252
Mesh <contact@devpush.io>
2026-04-14 17:59:24 +0100

    app

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,32 +5,18 @@ import { createApp, h } from 'vue'
 import '../css/app.css'
 //import AuthLayout from './layouts/AuthLayout.vue'
 //import UserLayout from './layouts/UserLayout.vue'
+import { createApp, h } from 'vue'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
-createInertiaApp(/*{
-  title: (title) => (title ? `${title} - ${appName}` : appName),
-  resolve: (name) => {
-    console.log('name', name);
-
-    const page: Promise<DefineComponent> = resolvePageComponent(
-      `./pages/${name}.vue`,
-      import.meta.glob<DefineComponent>('./pages/*.vue')
-    )
-
-    
-    page.then((module) => {
-      module.default.layout = name === 'Login' ? AuthLayout : UserLayout
-    })
-
-    return page
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue')
+    return pages[`./Pages/${name}.vue`]()
   },
   setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
+      createApp({ render: () => h(App, props) })
+          .use(plugin)
+          .mount(el)
   },
-  progress: {
-    color: '#4B5563',
-  },
-} */);
+});
